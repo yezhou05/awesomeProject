2f2dde5557b005da6a22e6c8cb5954f6f50cfc47
unknown <yezhou05@sina.com>
2020-09-11 17:59:29 +0800

    commit 20200911

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,13 @@ import (
 	"awesomeProject/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
+)
+
+// Eval is the main evaluation function
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -13,8 +20,17 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBooleanObject(node.Value)
 	}
 	return nil
+}
+
+func nativeBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
