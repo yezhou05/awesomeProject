c9003e6544561aec97243589d5009961c5ee995f
yezhou05 <yezhou05@sina.com>
2020-12-29 17:05:08 +0800

    commit 2020 finale

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -2,6 +2,7 @@ package evaluator
 
 import (
 	"awesomeProject/object"
+	"fmt"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -93,6 +94,15 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
